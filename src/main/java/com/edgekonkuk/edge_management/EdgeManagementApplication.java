7de0255d39fe1a2419400831221450a04b49503e
Dantt04 <jaejeong02@naver.com>
2025-09-05 19:03:01 +0900

    프로젝트 세팅

---
+++ src/main/java/com/edgekonkuk/edge_management/EdgeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.edgekonkuk.edge_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgekonkuk/edge_management/EdgeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgekonkuk.edge_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
